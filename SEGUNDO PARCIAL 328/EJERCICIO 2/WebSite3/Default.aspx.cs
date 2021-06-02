6081aff115cc58784cb7fbd3cf9bd21d34346eb6
Cristhianlq <62079292+Cristhianlq@users.noreply.github.com>
2021-06-02 00:23:51 -0400

    Add files via upload

---
+++ SEGUNDO PARCIAL 328/EJERCICIO 2/WebSite3/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ServiceReference1.WebServiceSoapClient sw = new ServiceReference1.WebServiceSoapClient();
+        String ci = TextBox1.Text;
+        GridView1.DataSource = sw.alumno(ci).Tables[0];
+        GridView1.DataBind();
+    }
+}
